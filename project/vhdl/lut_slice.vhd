81a7ce6defeafef3af18da735f51a65126cefe55
Samir Silbak <silbak04@gmail.com>
2013-10-18 13:58:05 -0400

    [vhdl] fixed shift register and muxes

--- project/vhdl/lut_slice.vhd
+++ project/vhdl/lut_slice.vhd
@@ -4,40 +4,43 @@ use ieee.numeric_std.all;
 
 entity lut_slice is
     generic(
-        delay : integer := 0 ps;
         bit_w : integer := 4
     );
     port(
-        a       : in std_logic;
-        b       : in std_logic;
-        d_in    : in std_logic;
-        clk     : in std_logic
+        clk : in std_logic;
+        di  : in std_logic;
+        a   : in std_logic;
+        b   : in std_logic;
+        q   : out std_logic_vector(bit_w-1 downto 0);
+        fo  : out std_logic
     );
 end entity lut_slice;
 
 architecture rtl of lut_slice is
 
-    signal q : std_logic_vector(bit_w-1 downto 0) := (others => '0');
+    signal qo : std_logic_vector(bit_w-1 downto 0) := (others => '0');
 
     signal f1 : std_logic := '0';
     signal f2 : std_logic := '0';
-    signal f3 : std_logic := '0';
 
 begin
 
-    process(clk, d_in) begin
+    process(clk, di) begin
         if rising_edge(clk) then
-            q <= (d_in sll 1);
+            qo(bit_w-1) <= di;
+            qo(bit_w-2 downto 0) <= qo(bit_w-1 downto 1);
         end if;
     end process;
 
-    f1 <= q(0) when (a = '0') else
-          q(1) when (a = '1');
+    q <= qo;
 
-    f2 <= q(2) when (a = '0') else
-          q(3) when (a = '1');
+    f1 <= qo(3) when (a = '0') else
+          qo(2) when (a = '1');
 
-    f3 <= f1   when (b = '0') else
-          f2   when (b = '1');
+    f2 <= qo(1) when (a = '0') else
+          qo(0) when (a = '1');
+
+    fo <= f1    when (b = '0') else
+          f2    when (b = '1');
 
 end rtl;
