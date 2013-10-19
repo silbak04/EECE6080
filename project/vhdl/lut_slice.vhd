f9ef4f271d6c2cf85146c01d9f5ef034e9d3d8b0
Samir Silbak <silbak04@gmail.com>
2013-10-19 16:23:15 -0400

    [vhdl] cleaned up a bit

--- project/vhdl/lut_slice.vhd
+++ project/vhdl/lut_slice.vhd
@@ -5,10 +5,10 @@ use ieee.numeric_std.all;
 entity lut_slice is
     port(
         clk_i   : in std_logic;
-        clk_o   : out std_logic;
         d       : in std_logic;
         a       : in std_logic;
         b       : in std_logic;
+        clk_o   : out std_logic;
         q       : out std_logic;
         f       : out std_logic
     );
@@ -23,66 +23,25 @@ architecture rtl of lut_slice is
 begin
 
     -- shifting in from MSB to LSB
-    ff1 : entity work.dffposx1
-    port map(
-        clk => clk_i,
-        d   => d,
-        q   => ff_o(3)
-    );
-
-    ff2 : entity work.dffposx1
-    port map(
-        clk => clk_i,
-        d   => ff_o(3),
-        q   => ff_o(2)
-    );
-
-    ff3 : entity work.dffposx1
-    port map(
-        clk => clk_i,
-        d   => ff_o(2),
-        q   => ff_o(1)
-    );
-
-    ff4 : entity work.dffposx1
-    port map(
-        clk => clk_i,
-        d   => ff_o(1),
-        q   => ff_o(0)
-    );
+    ff1 : entity work.dffposx1 port map(clk_i, d,       ff_o(3));
+    ff2 : entity work.dffposx1 port map(clk_i, ff_o(3), ff_o(2));
+    ff3 : entity work.dffposx1 port map(clk_i, ff_o(2), ff_o(1));
+    ff4 : entity work.dffposx1 port map(clk_i, ff_o(1), ff_o(0));
 
     -- lut shift out is output from prev ff
     q <= ff_o(0);
 
     -- select first two outputs of LUT
     -- on sel line A
-    mux1 : entity work.mux2x1
-    port map(
-        a => ff_o(3),
-        b => ff_o(2),
-        s => a,
-        x => mux_o(0)
-    );
+    mux1 : entity work.mux2x1 port map(ff_o(3), ff_o(2), a, mux_o(0));
 
     -- select last two outputs of LUT
     -- on sel line A
-    mux2 : entity work.mux2x1
-    port map(
-        a => ff_o(1),
-        b => ff_o(0),
-        s => a,
-        x => mux_o(1)
-    );
+    mux2 : entity work.mux2x1 port map(ff_o(1), ff_o(0), a, mux_o(1));
 
-    -- select last two outputs from
+    -- select the outputs from
     -- each mux on sel line B
-    mux3 : entity work.mux2x1
-    port map(
-        a => mux_o(0),
-        b => mux_o(1),
-        s => b,
-        x => f
-    );
+    mux3 : entity work.mux2x1 port map(mux_o(0), mux_o(1), b, f);
 
     clk_o <= clk_i;
 
