2241f25566acd4855455a5bb84e78286ec5d186c
Samir Silbak <silbak04@gmail.com>
2013-10-25 14:32:48 -0400

    [vhdl] added test mode in top

--- project/vhdl/top.vhd
+++ project/vhdl/top.vhd
@@ -19,26 +19,24 @@ end top;
 architecture rtl of top is
 
     signal shift_clk : std_logic := '0';
-    signal p_shf_out : std_logic := '0';
     signal l_shf_in  : std_logic := '0';
     signal p_out     : std_logic := '0';
+    signal p_clko    : std_logic := '0';
 
 begin
 
-    -- test mux connects output of P into input of LUT
-    -- and use same clk line
-    t_mux_1 : entity work.mux2x1 port map(l_clk, p_clk,     t_en, shift_clk);
-    t_mux_2 : entity work.mux2x1 port map(p_in,  p_out,     t_en, p_shf_out);
-    t_mux_3 : entity work.mux2x1 port map(l_in,  p_shf_out, t_en, l_shf_in);
+    -- test mux connects output of P into input of LUT and use same clock line
+    t_mux_1 : entity work.mux2x1 port map(l_clk, p_clko, t_en, shift_clk);
+    t_mux_2 : entity work.mux2x1 port map(l_in,  p_out,  t_en, l_shf_in);
 
     lut_1 : entity work.lut
     port map(
         s_clk   => p_clk,
         l_clk   => shift_clk,
-        s_in    => p_shf_out,
+        s_in    => p_in,
         l_in    => l_shf_in,
-        t_en    => t_en,
         t_po    => p_out,
+        t_co    => p_clko,
         f_o     => f_o,
         q_o     => q_o
     );
