942e233cfa6835b31e9d8a7bd80ad0e1157f1beb
Samir Silbak <silbak04@gmail.com>
2013-10-26 05:09:22 -0400

    [vdhl] added components and changed shift to happen from LSB to MSB

--- project/vhdl/lut_slice.vhd
+++ project/vhdl/lut_slice.vhd
@@ -16,32 +16,49 @@ end lut_slice;
 
 architecture rtl of lut_slice is
 
+    component dffposx1 is
+        port(
+            clk : in std_logic;
+            d   : in std_logic;
+            q   : out std_logic
+        );
+    end component;
+
+    component mux2x1 is
+        port(
+            a : in std_logic;
+            b : in std_logic;
+            s : in std_logic;
+            x : out std_logic
+        );
+    end component;
+
     -- flip flop and mux outputs
     signal ff_o  : std_logic_vector(3 downto 0) := (others => '0');
     signal mux_o : std_logic_vector(1 downto 0) := (others => '0');
 
 begin
 
-    -- shifting in from MSB to LSB
-    ff1 : entity work.dffposx1 port map(clk_i, d,       ff_o(3));
-    ff2 : entity work.dffposx1 port map(clk_i, ff_o(3), ff_o(2));
-    ff3 : entity work.dffposx1 port map(clk_i, ff_o(2), ff_o(1));
-    ff4 : entity work.dffposx1 port map(clk_i, ff_o(1), ff_o(0));
+    -- shifting in from LSB to MSB
+    ff1 : dffposx1 port map(clk_i, d,       ff_o(0));
+    ff2 : dffposx1 port map(clk_i, ff_o(0), ff_o(1));
+    ff3 : dffposx1 port map(clk_i, ff_o(1), ff_o(2));
+    ff4 : dffposx1 port map(clk_i, ff_o(2), ff_o(3));
 
     -- lut shift out is output from prev ff
-    q <= ff_o(0);
+    q <= ff_o(3);
 
     -- select first two outputs of LUT
     -- on sel line A
-    mux1 : entity work.mux2x1 port map(ff_o(3), ff_o(2), a, mux_o(0));
+    mux2 : mux2x1 port map(ff_o(0), ff_o(1), a, mux_o(0));
 
     -- select last two outputs of LUT
     -- on sel line A
-    mux2 : entity work.mux2x1 port map(ff_o(1), ff_o(0), a, mux_o(1));
+    mux1 : mux2x1 port map(ff_o(2), ff_o(3), a, mux_o(1));
 
     -- select the outputs from
     -- each mux on sel line B
-    mux3 : entity work.mux2x1 port map(mux_o(0), mux_o(1), b, f);
+    mux3 : mux2x1 port map(mux_o(0), mux_o(1), b, f);
 
     clk_o <= clk_i;
 
