94d8fb3b6831c1e3d707aa19ce3852a6908703ca
Samir Silbak <silbak04@gmail.com>
2013-10-27 00:31:31 -0400

    [vhdl] used dff's to generate my shift slice

--- project/vhdl/lut_slice.vhd
+++ project/vhdl/lut_slice.vhd
@@ -8,7 +8,6 @@ entity lut_slice is
         d       : in std_logic;
         a       : in std_logic;
         b       : in std_logic;
-        clk_o   : out std_logic;
         q       : out std_logic;
         f       : out std_logic
     );
@@ -34,32 +33,35 @@ architecture rtl of lut_slice is
     end component;
 
     -- flip flop and mux outputs
-    signal ff_o  : std_logic_vector(3 downto 0) := (others => '0');
-    signal mux_o : std_logic_vector(1 downto 0) := (others => '0');
+    signal ff_o     : std_logic_vector(4 downto 0) := (others => '0');
+    signal mux_o    : std_logic_vector(1 downto 0) := (others => '0');
 
 begin
 
     -- shifting in from LSB to MSB
-    ff1 : dffposx1 port map(clk_i, d,       ff_o(0));
-    ff2 : dffposx1 port map(clk_i, ff_o(0), ff_o(1));
-    ff3 : dffposx1 port map(clk_i, ff_o(1), ff_o(2));
-    ff4 : dffposx1 port map(clk_i, ff_o(2), ff_o(3));
+    shift_gen_lut : for i in 0 to 3 generate
+        ff_lut_i : dffposx1
+        port map(
+            clk => clk_i,
+            d   => ff_o(i),
+            q   => ff_o(i+1)
+        );
+    end generate;
 
     -- lut shift out is output from prev ff
-    q <= ff_o(3);
+    q <= ff_o(4);
+    ff_o(0) <= d;
 
     -- select first two outputs of LUT
     -- on sel line A
-    mux2 : mux2x1 port map(ff_o(0), ff_o(1), a, mux_o(0));
+    mux1 : mux2x1 port map(ff_o(1), ff_o(2), a, mux_o(0));
 
     -- select last two outputs of LUT
     -- on sel line A
-    mux1 : mux2x1 port map(ff_o(2), ff_o(3), a, mux_o(1));
+    mux2 : mux2x1 port map(ff_o(3), ff_o(4), a, mux_o(1));
 
     -- select the outputs from
     -- each mux on sel line B
     mux3 : mux2x1 port map(mux_o(0), mux_o(1), b, f);
-
-    clk_o <= clk_i;
 
 end rtl;
